2d8827ac415d5040c45a6c13f9fe3564bc2f869a
1113595204@qq.com <yanMING123>
2021-11-01 16:07:09 +0800

    Initial commit

---
+++ src/test/java/com/example/publicspring/PublicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publicspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
