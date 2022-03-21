faa90584966a45c5896a7888e9c7f0d9859fffed
1113595204@qq.com <yanMING1958>
2022-03-21 10:02:58 +0800

    Changes

---
+++ itcast-service-provider/src/main/java/controller/UserController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    @GetMapping("{id}")
+    public String queryById(@PathVariable("id")Long id){
+        return "id";
+    }
+}
