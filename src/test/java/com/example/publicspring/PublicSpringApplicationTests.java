a442dedc3666a5883e73144559844160bb125ca0
1113595204@qq.com <yanMING1958>
2022-09-16 10:31:39 +0800

    SXSSFWorkbook导出 并格式化单元格

--- src/test/java/com/example/publicspring/PublicSpringApplicationTests.java
+++ src/test/java/com/example/publicspring/PublicSpringApplicationTests.java
@@ -1,13 +1,58 @@
 package com.example.publicspring;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.streaming.SXSSFCell;
+import org.apache.poi.xssf.streaming.SXSSFRow;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @SpringBootTest
 class PublicSpringApplicationTests {
 
     @Test
     void contextLoads() {
-    }
 
+    }
+    public static void main(String[] args) throws IOException {
+       /* System.out.println(ss("q_", () -> "FD_DATA_INST='0000000'"));
+        consumerString(s -> System.out.println(s));
+        consumerString((obj) -> deal(obj));
+        导出格式化
+        */
+        String n = "123456.78901";
+        SXSSFWorkbook wb = new SXSSFWorkbook();
+        SXSSFSheet sheet = wb.createSheet();
+        sheet.setDefaultColumnWidth(20);
+        DataFormat format = wb.createDataFormat();
+        String[] formats = BuiltinFormats.getAll();
+        for (int i = 0; i < formats.length; i++) {
+            String f = formats[i];
+            System.out.println(f);
+            CellStyle style = wb.createCellStyle();
+            style.setAlignment(HorizontalAlignment.RIGHT);
+            style.setFillForegroundColor(IndexedColors.BLUE_GREY.getIndex());
+            style.setDataFormat(format.getFormat(f));
+            SXSSFRow row = sheet.createRow(i);
+            row.createCell(0).setCellValue(n);
+            SXSSFCell cell1 = row.createCell(1);
+            cell1.setCellValue(Double.valueOf(n));
+            cell1.setCellStyle(style);
+            row.createCell(2).setCellValue(f);
+        }
+        File file = new File("C:\\Users\\CF\\Downloads\\aa.xlsx");
+        if (file.exists()) {
+            file.delete();
+        }
+        FileOutputStream fos = new FileOutputStream(file);
+        wb.write(fos);
+        fos.flush();
+        fos.close();
+        wb.close();
+        System.out.println("结束");
+    }
 }
