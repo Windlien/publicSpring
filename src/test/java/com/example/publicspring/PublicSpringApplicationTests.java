663e847e5830afb0f20da09939e093b714cf23ce
Windlien <1113595204@qq.com>
2023-05-06 15:42:25 +0800

    将srcDir目录及子目录下的mp4文件复制到destDir下

--- src/test/java/com/example/publicspring/PublicSpringApplicationTests.java
+++ src/test/java/com/example/publicspring/PublicSpringApplicationTests.java
@@ -7,6 +7,7 @@ import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
--- src/test/java/com/example/publicspring/Test.java
+++ src/test/java/com/example/publicspring/Test.java
@@ -1,9 +1,18 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+package com.example.publicspring;
+
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        moveFiles();
+        //delDirNotEndWith();
+    }
+
+    private static void extracted() {
         List<Integer> list = Arrays.asList(2, 3, 1, 5);
         list.sort(new Comparator<Integer>() {
             @Override
@@ -21,13 +30,13 @@ public class Test {
         list.sort(Comparator.comparingInt(i -> i));
         System.out.println("2："+list);
         list.forEach(i -> System.out.print("元素："+i));
-    //实例3：把Lambda赋值给变量
+        //实例3：把Lambda赋值给变量
         Runnable task = ()->{
             System.out.println("\nhello lambda!");
         };
         new Thread(task).start();
-
     }
+
     public  void main1(String[] args) {
         String s1 = "111111111111111111111111111";
         int i1 = 1000000000;
@@ -39,5 +48,36 @@ public class Test {
             System.out.println("list1个数" + list1.size());
         }
         list1.forEach(System.out::println);
+    }
+    public static void moveFiles() throws IOException, InterruptedException {
+        String srcDir = "";
+        String destDir = "";
+        Collection<File> srcFiles = FileUtils.listFiles(new File(srcDir),null,true).stream().sorted().collect(Collectors.toList());
+        for (File srcFfile : srcFiles) {
+            if (srcFfile.exists()&&srcFfile.getName().toUpperCase().endsWith(".MP4")) {
+                File destFile = new File(destDir+ File.separator+srcFfile.getName());
+                if (destFile.exists()) {
+                    destFile.delete();
+                    FileUtils.copyFileToDirectory(srcFfile,new File(destDir),true);
+                }else {
+                    System.out.println(srcFfile.getName());
+                    FileUtils.copyFileToDirectory(srcFfile,new File(destDir),true);
+                }
+                Thread.sleep(300);
+            }
+        }
+        System.out.println("结束");
+    }
+    public static void delDirNotEndWith() throws IOException {
+        String srcDir = "";
+        String suffix=".mp4";
+        File[] srcD = new File(srcDir).listFiles();
+        for (File file : srcD) {
+            Collection<File> files = FileUtils.listFiles(file, new String[]{suffix}, false);
+            if (files.isEmpty()){
+                System.out.println(file.getName());
+                FileUtils.deleteDirectory(file);
+            }
+        }
     }
 }
