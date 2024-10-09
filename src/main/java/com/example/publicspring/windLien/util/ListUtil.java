c7985d60f91958c40582f0ae25cd8f35ed5e944f
Windlien <1113595204@qq.com>
2024-10-09 10:28:45 +0800

    一个工具类

--- src/main/java/com/example/publicspring/windLien/util/ListUtil.java
+++ src/main/java/com/example/publicspring/windLien/util/ListUtil.java
@@ -16,14 +16,14 @@ public class ListUtil {
         return l;
     }
 
-    @SuppressWarnings("unchecked")
-    public static List<String> toLowerCase(Collection<String> collection) {
-        List<String> low = new ArrayList<>();
-        for (String col : collection) {
-            low.add(col.toLowerCase());
-        }
-        return low;
-    }
+//    @SuppressWarnings("unchecked")
+//    public static List<String> toLowerCase(Collection<String> collection) {
+//        List<String> low = new ArrayList<>();
+//        for (String col : collection) {
+//            low.add(col.toLowerCase());
+//        }
+//        return low;
+//    }
 
     @SuppressWarnings("unchecked")
     public static List<String> toUpperCase(Collection<String> collection) {
@@ -48,5 +48,17 @@ public class ListUtil {
         return filtered;
     }
 
-
+    /**
+     * 针对sql查询列表过滤目标列表，同时去掉目标列
+     */
+    public static List<Map<String, Object>> filterList2(List<Map<String, Object>> source, String fieldName, String value) {
+        List<Map<String, Object>> filtered = new ArrayList<Map<String, Object>>();
+        source.stream().filter(map -> {
+            return value.equals(map.get(fieldName));
+        }).forEach(map -> {
+            map.remove(fieldName);
+            filtered.add(map);
+        });
+        return filtered;
+    }
 }
