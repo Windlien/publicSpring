2467bcba95ad383af6cd911e42673396c1614081
Windlien <1113595204@qq.com>
2025-03-04 10:31:23 +0800

    冒泡排序算法

--- src/test/java/com/example/publicspring/Test.java
+++ src/test/java/com/example/publicspring/Test.java
@@ -185,9 +185,39 @@ public class Test {
         }
         return new int[0];
     }
+
+    // 冒泡排序算法
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // 交换 arr[j] 和 arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // 打印数组
+    static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int[] ints = twoSum(new int[]{2, 7, 11, 15}, 9);
         System.out.println(ints);
+
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(array);
+        System.out.println("Sorted array:");
+        printArray(array);
     }
 
 }
